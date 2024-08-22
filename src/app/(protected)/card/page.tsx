9b2e09caec600eb16a525d5322913486f804a014
Nelson Izquierdo <izquierdonelson@gmail.com>
2024-08-21 23:21:46 -0400

    chore: Update card page layout and functionality

---
+++ src/app/(protected)/card/page.tsx
@@ -0,0 +1,43 @@
+import { auth } from "@/auth";
+import Confetti from "@/components/Confetti";
+import { range } from "@/lib";
+import { IconCoffee } from "@tabler/icons-react";
+
+export default async function CardPage({ params }: { params: { id: string } }) {
+  const session = await auth();
+  console.log(session);
+  const { id } = params;
+  const coffees = Math.floor(Math.random() * 8) + 1;
+
+  return (
+    <div>
+      <header className="bg-green-900 text-white flex flex-col gap-y-4 p-4">
+        <h1 className="text-6xl text-balance">The Reset Club</h1>
+        <p className="text-xs">
+          Hola!{" "}
+          <span className="font-bold text-sm"> {session?.user?.name}</span>,
+          tienes <span className="font-bold text-sm">{coffees}</span> de 8 cafés
+          para reclamar tu{" "}
+          <span className="font-bold text-sm">café gratis</span>
+        </p>
+      </header>
+      {coffees === 8 ? <Confetti /> : null}
+      <div className="grid grid-cols-2 gap-4 p-3">
+        {range(8).map((_, i) => {
+          if (i < coffees) {
+            return (
+              <div key={i} className="grid justify-center">
+                <IconCoffee stroke={1.5} size={50} />
+              </div>
+            );
+          }
+          return (
+            <div key={i} className="grid opacity-20 justify-center">
+              <IconCoffee stroke={1} size={50} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
