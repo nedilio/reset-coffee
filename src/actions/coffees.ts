bc0b753fc2c91e41437cab60fb43897e5921b4d0
Nelson Izquierdo <izquierdonelson@gmail.com>
2024-12-30 19:56:03 -0500

    feat: Add Vercel Analytics tracking for coffee purchase events

--- src/actions/coffees.ts
+++ src/actions/coffees.ts
@@ -1,6 +1,7 @@
 "use server";
 import { TABLE_NAME } from "@/lib/constants";
 import { supabase } from "@/supabase.config";
+import { track } from "@vercel/analytics";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
@@ -8,6 +9,7 @@ export const addCoffee = async (formData: FormData) => {
   const id = formData.get("id") as string;
   const coffees = parseInt(formData.get("coffees") as string);
   await supabase.from(TABLE_NAME).upsert({ id, coffees: coffees + 1 });
+  track("Cafe Comprado", { id });
   revalidatePath("/admin");
 };
 
