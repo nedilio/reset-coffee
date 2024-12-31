a77c65789f1efbdbf2640bfe0f6d589449c0a9ea
Nelson Izquierdo <izquierdonelson@gmail.com>
2024-12-30 19:50:09 -0500

    feat: Integrate Vercel Analytics tracking on user login

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,6 +3,7 @@ import Google from "next-auth/providers/google";
 import { supabase } from "./supabase.config";
 import { removeAccents } from "./lib/utils";
 import { TABLE_NAME } from "./lib/constants";
+import { track } from "@vercel/analytics";
 
 const adminEmails = [
   "izquierdonelson@gmail.com",
@@ -46,6 +47,12 @@ export const authConfig = {
       session.user.role = role;
       const { email, name, image } = session.user;
       const normalizedName = removeAccents(name ?? "");
+      track("Inicia Sesion", {
+        email,
+        name: normalizedName,
+        image: image ?? "",
+        role,
+      });
 
       const { data, error } = await supabase
         .from(TABLE_NAME)
