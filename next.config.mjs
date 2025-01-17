fbdd571662a192be1a559a14b76ff3fe54f2c567
Nelson Izquierdo <izquierdonelson@gmail.com>
2025-01-17 16:35:33 -0500

    feat: Update PostHog client host and add new ingestion endpoint rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         source: "/ingest/decide",
         destination: "https://us.i.posthog.com/decide",
       },
+      {
+        source: "/e/:path*",
+        destination: "https://us.i.posthog.com/e/:path*",
+      },
     ];
   },
   // This is required to support PostHog trailing slash API requests
