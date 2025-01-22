0efbe0223269fd46b0a3a136f7cbd25f38e317ba
Nelson Izquierdo <izquierdonelson@gmail.com>
2025-01-21 21:07:15 -0500

    feat: Remove capture_pageview and autocapture from PostHog initialization; update card_viewed event to use $pathname

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -3,12 +3,10 @@ import posthog from "posthog-js";
 const CardView = ({ email }: { email: string }) => {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    capture_pageview: false,
     capture_pageleave: false,
-    autocapture: false,
   });
   posthog.people.set({ email });
-  posthog.capture("card_viewed", { email, $url: "/card" });
+  posthog.capture("card_viewed", { email, $pathname: "/card" });
   return null;
 };
 
