795b23e92844d1a90d2e83b8503f2826837aa382
Nelson Izquierdo <izquierdonelson@gmail.com>
2025-01-21 20:41:39 -0500

    feat: Update PostHog provider to use environment variable for API host

--- src/app/providers/ph-provider.tsx
+++ src/app/providers/ph-provider.tsx
@@ -4,10 +4,8 @@ import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host:
-      "https://reset-coffee-git-develop-nedilios-projects.vercel.app/analytics",
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     capture_pageview: false,
-
     capture_pageleave: false,
   });
 }
