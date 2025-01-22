4941738414f80600031f5f6a612e6907bd65e911
Nelson Izquierdo <izquierdonelson@gmail.com>
2025-01-21 21:39:11 -0500

    feat: Remove PostHog initialization from CardView component

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -1,10 +1,6 @@
 import posthog from "posthog-js";
 
 const CardView = ({ email }: { email: string }) => {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    capture_pageleave: false,
-  });
   posthog.people.set({ email });
   posthog.capture("card_viewed", { email, $pathname: "/card" });
   return null;
